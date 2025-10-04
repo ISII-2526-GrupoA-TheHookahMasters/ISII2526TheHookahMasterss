d9bf9d2a55cfd021d295a236a976f67b03142ca2
IvanGomez11 <ivan.gomez11@alu.uclm.es>
2025-10-04 13:45:18 +0200

    Clase Herramienta creada

---
+++ src/AppForSEII2526.API/Models/Herramienta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Herramienta
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "El campo Nombre es obligatorio")]
+    [StringLength(100, ErrorMessage = "Longitud máxima de 100 caracteres superada")]
+    public string Nombre { get; set; }
+
+    [Required(ErrorMessage = "El campo Material es obligatorio")]
+    [StringLength(100, ErrorMessage = "Longitud máxima de 100 caracteres superada")]
+    public string Material { get; set; }
+
+    [Required(ErrorMessage = "El campo Precio es obligatorio")]
+    [Range(0, double.MaxValue, ErrorMessage = "El Precio no puede ser negativo")]
+    public decimal Precio { get; set; }
+
+    public int TiempoReparacion { get; set; }
+}
