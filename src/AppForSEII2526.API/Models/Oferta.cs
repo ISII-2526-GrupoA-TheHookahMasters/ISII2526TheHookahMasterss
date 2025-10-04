64475555ffd516e6025fe3db81f4199bef5ba051
IvanGomez11 <ivan.gomez11@alu.uclm.es>
2025-10-04 12:04:03 +0200

    Clase Oferta creada (#7)

---
+++ src/AppForSEII2526.API/Models/Oferta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Oferta
+{
+	[Key]
+	public int Id { get; set; }
+
+	[Required]
+    [DataType(System.ComponentModel.DataAnnotations.DataType.Date), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+    public DateTime FechaFinal { get; set; }
+
+    [Required]
+    [DataType(System.ComponentModel.DataAnnotations.DataType.Date), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+    public DateTime FechaInicio { get; set; }
+
+    [Required]
+    [DataType(System.ComponentModel.DataAnnotations.DataType.Date), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+    public DateTime FechaOferta { get; set; }
+}
