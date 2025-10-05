c7e4b68d8fa72d26c2e30839a2116e8ac006d3be
IvanGomez11 <ivan.gomez11@alu.uclm.es>
2025-10-05 19:59:09 +0200

    Clase Fabricante creada (#22)

---
+++ src/AppForSEII2526.API/Models/Fabricante.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Fabricante
+{
+	[Key]
+	public int Id { get; set; }
+
+    [Required(ErrorMessage = "El campo Nombre es obligatorio")] 
+    [StringLength(100, ErrorMessage = "Error: Longitud máxima permitida de 100 caracteres")]
+    public string Nombre { get; set; }
+}
