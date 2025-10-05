4931934312e26a98394b570a78e657a302a08d24
IvanGomez11 <ivan.gomez11@alu.uclm.es>
2025-10-05 21:04:28 +0200

    Relaciones de la clase Herramienta creadas, clase añadida al DbContext

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -6,6 +6,7 @@ namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
 
+    public DbSet<Herramienta> Herramienta { get; set; }
     public DbSet<CompraItem> CompraItem { get; set; }
     public DbSet<AlquilarItem> AlquilarItem { get; set; }
     public DbSet<Compra> Compra { get; set; }
--- src/AppForSEII2526.API/Models/Herramienta.cs
+++ src/AppForSEII2526.API/Models/Herramienta.cs
@@ -13,9 +13,16 @@ public class Herramienta
     [StringLength(100, ErrorMessage = "Longitud máxima de 100 caracteres superada")]
     public string Material { get; set; }
 
-    [Required(ErrorMessage = "El campo Precio es obligatorio")]
-    [Range(0, double.MaxValue, ErrorMessage = "El Precio no puede ser negativo")]
+    [DataType(DataType.Currency)]
+    [Display(Name = "Precio de la Herramienta")]
     public decimal Precio { get; set; }
 
     public int TiempoReparacion { get; set; }
+
+    //Relaciones
+    public List<CompraItem> CompraItem { get; set; }
+    public List<AlquilarItem> AlquilarItem { get; set; }
+    public List<OfertaItem> OfertaItem { get; set; }
+    public List<ReparacionItem> ReparacionItem { get; set; }
+    public Fabricante Fabricante { get; set; }
 }
