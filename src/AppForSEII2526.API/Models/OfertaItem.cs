d79d937d729ccdfedeb0344d8a54fdfa46ec5b95
IvanGomez11 <ivan.gomez11@alu.uclm.es>
2025-10-05 19:48:48 +0200

    Clase OfertaItem creada (#10)

---
+++ src/AppForSEII2526.API/Models/OfertaItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class OfertaItem
+{
+	[Key]
+	public int IdOferta { get; set; }
+
+	[Required]
+    public int IdHerramienta { get; set; }
+
+	[Required]
+    public int Porcentaje { get; set; }
+
+	public float PrecioFinal { get; set; }
+}
