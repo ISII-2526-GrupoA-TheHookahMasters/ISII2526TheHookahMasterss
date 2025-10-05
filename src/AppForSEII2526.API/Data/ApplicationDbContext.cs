f9b07882480ad53228970788379018643b3cc4f9
sreque_ <152419135+SergioReque@users.noreply.github.com>
2025-10-05 19:46:59 +0200

    Creada clase CompraItem y añadida a DbContext. (#20)

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,5 +5,7 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+
+    public DbSet<CompraItem> CompraItem { get; set; }
     public DbSet<AlquilarItem> AlquilarItem { get; set; }
 }
