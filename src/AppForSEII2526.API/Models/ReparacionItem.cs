d488d322ec873b3cf17449ca30d0acbfc6e90db5
Kikolonx <joseenrique.ortiz@alu.uclm.es>
2025-10-06 10:14:39 +0200

    Crear clase reparacionitem (#48)
    * #15 Prrimera version de la clase ReparacionItem
    
    * #Clase ReparacionItem creada
    
    ---------
    
    Co-authored-by: IvanGomez11 <ivan.gomez11@alu.uclm.es>

---
+++ src/AppForSEII2526.API/Models/ReparacionItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class ReparacionItem
+{
+    [Key]
+    public int IdReparacion { get; set; }
+
+    [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "Cantidad minima de compra es 1")]
+    public int Cantidad { get; set; }
+
+    public string Descripcion { get; set; }
+
+    [Required]
+    public int IdHerramienta { get; set; }
+
+    [Required]
+    [DataType(DataType.Currency)]
+    [Display(Name = "Precio")]
+    public float Precio { get; set; }
+}
