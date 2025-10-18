540535db7c9dbda54e6f18b22ad2265039716f80
SERGIO REQUENA MEGÍAS <Sergio.Requena2@alu.uclm.es>
2025-10-18 11:02:02 +0200

    Implementación del método GET de la selección para el Caso de Uso Compra

---
+++ src/AppForSEII2526.API/Controllers/HerramientaController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AppForSEII2526.API.Data;
+using AppForSEII2526.API.DTOs;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HerramientaController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        private readonly ILogger<HerramientaController> _logger;
+
+        public HerramientaController(ApplicationDbContext context, ILogger<HerramientaController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // Devuelve la informacion directamente de la BBDD
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<Herramienta>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetHerramientas()
+        {
+            IList<Herramienta> herramientas = await _context.Herramienta
+                .ToListAsync();
+            return Ok(herramientas);
+        }
+
+        // Devuelve la informacion mapeada a DTOs
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<HerramientasParaComprarDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetHerramientas_conDTOs()
+        {
+            var herramientas = await _context.Herramienta
+                .Select(h => new HerramientasParaComprarDTO(h.Id, h.Nombre, h.Material, h.Precio, h.Fabricante.Nombre))
+                .ToListAsync();
+            return Ok(herramientas);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<HerramientasParaComprarDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetHerramientas_filtroMaterialPrecioDTOs(string? filtroMaterial, double? filtroPrecio)
+        {
+            IList<HerramientasParaComprarDTO> herramientas = await _context.Herramienta
+                .Include(h => h.Fabricante)
+                .Where(h => (filtroMaterial == null || h.Material.Contains(filtroMaterial)) 
+                    && (filtroPrecio == null || h.Precio <= filtroPrecio))
+                .OrderBy(h => h.Precio)
+                .Select(h => new HerramientasParaComprarDTO(h.Id, h.Nombre, h.Material, h.Precio, h.Fabricante.Nombre))
+                .ToListAsync();
+            return Ok(herramientas);
+        }
+
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/HerramientasParaComprarDTO.cs
@@ -0,0 +1,42 @@
+﻿namespace AppForSEII2526.API.DTOs
+{
+    public class HerramientasParaComprarDTO
+    {
+        [Key]
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [JsonPropertyName("nombre")]
+        [StringLength(100, ErrorMessage = "Longitud máxima de 100 caracteres superada")]
+        public string Nombre { get; set; }
+
+        [Required]
+        [JsonPropertyName("material")]
+        [StringLength(100, ErrorMessage = "Longitud máxima de 100 caracteres superada")]
+        public string Material { get; set; }
+
+        [Required]
+        [JsonPropertyName("precio")]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency), Display(Name = "Precio de la Herramienta")]
+        public float Precio { get; set; }
+
+        [Required]
+        [JsonPropertyName("fabricante")]
+        public string Fabricante { get; set; }
+
+
+        public HerramientasParaComprarDTO(int id, string nombre, string material, float precio, string fabricante)
+        {
+            Id = id;
+            Nombre = nombre;
+            Material = material;
+            Precio = precio;
+            Fabricante = fabricante;
+        }
+
+    }
+
+
+
+}
