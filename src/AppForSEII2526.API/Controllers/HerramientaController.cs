c8fd78b40c411bc2ed0e97683e1aa07ff1e85558
Kikolonx <joseenrique.ortiz@alu.uclm.es>
2025-10-18 10:57:45 +0200

    #Implementacion del metodo GET de la seleccion para el caso de uso de Reparacion

---
+++ src/AppForSEII2526.API/Controllers/HerramientaController.cs
@@ -0,0 +1,58 @@
+﻿using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HerramientaController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        private readonly ILogger<HerramientaController> _logger;
+
+        public HerramientaController(ApplicationDbContext context, ILogger<HerramientaController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<Herramienta>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetHerramientas()
+        {
+            IList<Herramienta> herramientas = await _context.Herramienta
+                .ToListAsync();
+            return Ok(herramientas);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<HerramientasParaRepararDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetHerramientasParaReparar()
+        {
+            var herramientas = await _context.Herramienta
+                .Select(h => new HerramientasParaRepararDTO(h.Id, h.Nombre, h.Material, h.Precio, h.Fabricante.Nombre, h.TiempoReparacion))
+                .ToListAsync();
+            return Ok(herramientas);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<HerramientasParaRepararDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetHerramientas(string nombre, string tiemporeparacion)
+        {
+            IList<HerramientasParaRepararDTO> selectHerramientas = await _context.Herramienta
+                .Where(h => h.Nombre.Contains(nombre) 
+                       && h.TiempoReparacion.ToString().Contains(tiemporeparacion))
+               
+                .Select(h => new HerramientasParaRepararDTO(h.Id, h.Nombre, h.Material, h.Precio, h.Fabricante.Nombre, h.TiempoReparacion))
+
+                .ToListAsync();
+
+            return Ok(selectHerramientas);
+        }
+    }
+}
