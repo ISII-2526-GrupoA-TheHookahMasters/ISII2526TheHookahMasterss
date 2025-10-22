25ad826887747ecb6957a7366cb7e5a41ffbbac5
SERGIO REQUENA MEGÍAS <Sergio.Requena2@alu.uclm.es>
2025-10-22 12:32:41 +0200

    Implementacion deL metodo GET del detalle del CU Comprar herramientas

---
+++ src/AppForSEII2526.API/Controllers/CompraController.cs
@@ -0,0 +1,61 @@
+﻿using AppForSEII2526.API.DTOs;
+using AppForSEII2526.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompraController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        private readonly ILogger<CompraController> _logger;
+
+        public CompraController(ApplicationDbContext context, ILogger<CompraController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<CompraDetailDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetComprasPorId(int id)
+        {
+            var compra = await _context.Compra
+                .Where(o => o.Id == id)
+                    .Include(o => o.CompraItems)
+                        .ThenInclude(oi => oi.Herramienta)
+                            .ThenInclude(h => h.Fabricante)
+
+                .Select(o => new CompraDetailDTO(
+                    o.Id,
+                    o.Usuario.Nombre,
+                    o.Usuario.Apellido,
+                    o.DireccionEnvio,
+                    o.FechaCompra,
+                    o.PrecioTotal,
+                    o.CompraItems   
+                    
+                        .Select(oi => new CompraItemDTO(
+                            oi.CompraId,
+                            oi.Herramienta.Nombre,
+                            oi.Herramienta.Material,
+                            oi.Precio,
+                            oi.Descripcion,
+                            oi.Cantidad)).ToList<CompraItemDTO>()))
+                
+                .FirstOrDefaultAsync();
+
+            if (compra == null)
+            {
+                _logger.LogError($"Error: Compra con id {id} no existe");
+                return NotFound();
+            }
+
+            return Ok(compra);
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/CompraItemDTO.cs
@@ -0,0 +1,43 @@
+﻿namespace AppForSEII2526.API.DTOs
+{
+    public class CompraItemDTO
+    {
+        [JsonPropertyName("compraId")]
+        public int CompraId { get; set; }
+
+        [JsonPropertyName("nombreHerramienta")]
+        [Required]
+        [StringLength(100, ErrorMessage = "Longitud máxima de 100 caracteres superada")]
+        public string NombreHerramienta { get; set; }
+
+        [JsonPropertyName("materialHerramienta")]
+        [Required]
+        [StringLength(100, ErrorMessage = "Longitud máxima de 100 caracteres superada")]
+        public string Material { get; set; }
+
+        [JsonPropertyName("precioHerramienta")]
+        [Required]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency), Display(Name = "Precio de la Herramienta")]
+        public float Precio { get; set; }
+
+        [JsonPropertyName("descripcionHerramienta")]
+        [Required]
+        public string Descripcion { get; set; }
+
+        [JsonPropertyName("cantidad")]
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Cantidad minima de compra es 1")]
+        public int Cantidad { get; set; }
+
+
+        public CompraItemDTO(int compraId, string nombreHerramienta, string material, float precio, string descripcion, int cantidad)
+        {
+            CompraId = compraId;
+            NombreHerramienta = nombreHerramienta;
+            Material = material;
+            Precio = precio;
+            Descripcion = descripcion;
+            Cantidad = cantidad;
+        }
+    }
+}
