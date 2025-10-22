ed47abc2a6c2ae84def2cdfb6fbc97907c145046
Iván Gómez <ivan.gomez11@alu.uclm.es>
2025-10-22 09:40:25 +0200

    Implementación del método GET del detalle del Caso de Uso Crear Ofertas (#102)

---
+++ src/AppForSEII2526.API/Controllers/OfertaController.cs
@@ -0,0 +1,48 @@
+﻿using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OfertaController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        private readonly ILogger<OfertaController> _logger;
+
+        public OfertaController(ApplicationDbContext context, ILogger<OfertaController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<OfertaDetailDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetOfertasPorId(int id)
+        {
+            var oferta = await _context.Oferta
+                .Where(o => o.Id == id)
+                    .Include(o => o.OfertaItems)
+                        .ThenInclude(oi => oi.Herramienta)
+                            .ThenInclude(h => h.Fabricante)
+                            
+                .Select(o => new OfertaDetailDTO(o.Id, o.FechaFinal, o.FechaInicio, o.FechaOferta,
+                o.TipoMetodoPago, o.TipoDirigidaOferta, o.OfertaItems
+                            .Select(oi => new OfertaItemDTO(oi.OfertaId, oi.PrecioFinal, oi.Herramienta.Nombre, 
+                                oi.Herramienta.Precio, oi.Herramienta.Material, 
+                                oi.Herramienta.Fabricante.Nombre)).ToList<OfertaItemDTO>()))
+                .FirstOrDefaultAsync();
+
+            if (oferta == null)
+            {
+                _logger.LogError($"Error: Oferta con id {id} no existe");
+                return NotFound();
+            }
+
+            return Ok(oferta);
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/OfertaDetailDTO.cs
@@ -0,0 +1,47 @@
+﻿namespace AppForSEII2526.API.DTOs
+{
+    public class OfertaDetailDTO
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date), Display(Name = "Fecha Final")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [JsonPropertyName("fechaFinal")]
+        public DateTime FechaFinal { get; set; }
+
+        [Required]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date), Display(Name = "Fecha Inicio")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [JsonPropertyName("fechaInicio")]
+        public DateTime FechaInicio { get; set; }
+
+        [Required]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date), Display(Name = "Fecha Oferta")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [JsonPropertyName("fechaOferta")]
+        public DateTime FechaOferta { get; set; }
+
+        [Required]
+        [JsonPropertyName("tipoMetodoPago")]
+        public TiposMetodoPago TipoMetodoPago { get; set; }
+
+        [JsonPropertyName("tipoDirigidaOferta")]
+        public TiposDirigidaOferta? TipoDirigidaOferta { get; set; }
+
+        public IList<OfertaItemDTO> OfertaItemDTO { get; set; }
+
+        public OfertaDetailDTO(int id, DateTime fechaFinal, DateTime fechaInicio, DateTime fechaOferta,
+                         TiposMetodoPago tipoMetodoPago, TiposDirigidaOferta? tipoDirigidaOferta, IList<OfertaItemDTO> ofertaItemDTO)
+        {
+            Id = id;
+            FechaFinal = fechaFinal;
+            FechaInicio = fechaInicio;
+            FechaOferta = fechaOferta;
+            TipoMetodoPago = tipoMetodoPago;
+            TipoDirigidaOferta = tipoDirigidaOferta;
+            OfertaItemDTO = ofertaItemDTO;
+        }
+    }
+}
