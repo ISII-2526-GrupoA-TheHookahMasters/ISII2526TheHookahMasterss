3f14a5ed0653ec76e7a6e2f7ebb0bd5b118b480b
IvanGomez11 <ivan.gomez11@alu.uclm.es>
2025-11-06 12:14:20 +0100

    Implementadas las pruebas de unidad para el método POST del Caso de Uso Oferta y arreglado el precioFInal

--- src/AppForSEII2526.API/DTOs/OfertaDTOs/OfertaItemDTO.cs
+++ src/AppForSEII2526.API/DTOs/OfertaDTOs/OfertaItemDTO.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using AppForSEII2526.API.Models;
+
 namespace AppForSEII2526.API.DTOs.OfertaDTOs
 {
     public class OfertaItemDTO
@@ -7,20 +8,9 @@ namespace AppForSEII2526.API.DTOs.OfertaDTOs
         public int HerramientaId { get; set; }
 
         [Required]
-        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
-        [Display(Name = "Precio final de la oferta")]
-        [JsonPropertyName("precioFinal")]
-        public float PrecioFinal { get; set; }
-
-        [Required]
         [StringLength(100, ErrorMessage = "Longitud máxima de 100 caracteres superada")]
         [JsonPropertyName("nombreHerramienta")]
         public string NombreHerramienta { get; set; }
-
-        [Required]
-        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency), Display(Name = "Precio de la Herramienta")]
-        [JsonPropertyName("precio")]
-        public float Precio { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "Longitud máxima de 100 caracteres superada")]
@@ -30,32 +20,52 @@ namespace AppForSEII2526.API.DTOs.OfertaDTOs
         [JsonPropertyName("fabricante")]
         public string Fabricante { get; set; }
 
-        public OfertaItemDTO(int herramientaId, float precioFinal, string nombreHerramienta,
-                         float precio, string material, string fabricante)
+        [Required]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency), Display(Name = "Precio de la Herramienta")]
+        [JsonPropertyName("precio")]
+        public float Precio { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "El porcentaje debe estar entre 0 y 100")]
+        [JsonPropertyName("porcentaje")]
+        public int Porcentaje { get; set; }
+
+        [Required]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Display(Name = "Precio final de la oferta")]
+        [JsonPropertyName("precioFinal")]
+        public float PrecioFinal { 
+            get {
+                return Precio * (1 - (Porcentaje / 100.0f));
+            } 
+        }
+
+        public OfertaItemDTO(int herramientaId, string nombreHerramienta, string material,
+                           string fabricante, float precio, int porcentaje)
         {
             HerramientaId = herramientaId;
-            PrecioFinal = precioFinal;
             NombreHerramienta = nombreHerramienta;
-            Precio = precio;
             Material = material;
             Fabricante = fabricante;
+            Precio = precio;
+            Porcentaje = porcentaje;
         }
 
         public override bool Equals(object? obj)
         {
             return obj is OfertaItemDTO dTO &&
                    HerramientaId == dTO.HerramientaId &&
-                   PrecioFinal == dTO.PrecioFinal &&
                    NombreHerramienta == dTO.NombreHerramienta &&
-                   Precio == dTO.Precio &&
                    Material == dTO.Material &&
-                   Fabricante == dTO.Fabricante;
+                   Fabricante == dTO.Fabricante &&
+                   Precio == dTO.Precio &&
+                   Porcentaje == dTO.Porcentaje &&
+                   PrecioFinal == dTO.PrecioFinal;
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(HerramientaId, PrecioFinal, NombreHerramienta, Precio, Material, Fabricante);
+            return HashCode.Combine(HerramientaId, NombreHerramienta, Material, Fabricante, Precio, Porcentaje, PrecioFinal);
         }
     }
 }
-
