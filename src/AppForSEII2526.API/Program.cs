5b479a6d663f80512c0d7707f8fe69b0c6e9c8bc
IvanGomez11 <ivan.gomez11@alu.uclm.es>
2025-11-07 08:42:47 +0100

    Añadida toda la lógica para el Logging con RabbitMQ

--- src/AppForSEII2526.API/Program.cs
+++ src/AppForSEII2526.API/Program.cs
@@ -1,7 +1,11 @@
+using AppForSEII2526.API.Logging;
 using Microsoft.Data.Sqlite;
 using System.Data.Common;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddRabbitMQ(builder.Configuration.GetSection("RabbitMQ"));
+//”RabbitMQ” coincide con el nombre del bloque de propiedades en appsettings.json
 
 // Add services to the container.
 
@@ -115,4 +119,4 @@ app.MapControllers();
 app.Run();
 
 //Expose the implicitly defined Program class to the test project by doing:
-public partial class Program { }
+public partial class Program { } 
