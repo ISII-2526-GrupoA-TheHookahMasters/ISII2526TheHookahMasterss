4abdedeccf3244ced63266187025c6c442f96a3c
Kikolonx <joseenrique.ortiz@alu.uclm.es>
2025-11-25 16:33:03 +0100

    Creado ReparacionStateContainer (#181)

---
+++ src/AppForSEII2526.Web/ReparacionStateContainer.cs
@@ -0,0 +1,61 @@
+﻿using AppForSEII2526.Web.API;
+
+namespace AppForSEII2526.Web
+{
+    public class ReparacionStateContainer
+    {
+        public ReparacionForCreateDTO Reparacion { get; private set; } = new ReparacionForCreateDTO()
+        {
+            ReparacionItems = new List<ReparacionItemDTO>()
+        };
+
+        public float PrecioTotal
+        {
+            get
+            {
+                int numberOfDays = (Reparacion.FechaRecogida - Reparacion.FechaEntrega).Days;
+                return (float)Reparacion.ReparacionItems.Sum(ri => ri.Precio * numberOfDays);
+            }
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+
+        public void AddHerramientaToReparacion(HerramientasParaRepararDTO herramienta)
+        {
+            if (!Reparacion.ReparacionItems.Any(ri => ri.HerramientaId == herramienta.Id))
+                Reparacion.ReparacionItems.Add(new ReparacionItemDTO()
+                {
+                    HerramientaId = herramienta.Id,
+                    NombreHerramienta = herramienta.Nombre,
+                    Precio = herramienta.Precio,
+                }
+            );
+        }
+
+
+
+        public void RemoveReparacionItemToReparar(ReparacionItemDTO item)
+        {
+            Reparacion.ReparacionItems.Remove(item);
+
+        }
+
+        public void ClearReparacionCart()
+        {
+            Reparacion.ReparacionItems.Clear();
+        }
+
+        public void ReparacionProcessed()
+        {
+            //we have finished the rental process so we create a new object without data
+            Reparacion = new ReparacionForCreateDTO()
+            {
+                ReparacionItems = new List<ReparacionItemDTO>()
+            };
+        }
+
+    }
+}
