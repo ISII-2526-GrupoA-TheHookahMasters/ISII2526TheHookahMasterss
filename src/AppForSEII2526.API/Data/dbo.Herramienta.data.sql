a2c61b0af9c7909961ee7cb235f7ed2c99f193de
AlejandroNvarrete <alejandro.navarrete@alu.uclm.es>
2025-12-13 09:50:50 +0100

    Unificada toda la base de datos en un solo archivo .sql (#220)

--- src/AppForSEII2526.API/Data/dbo.Herramienta.data.sql
+++
@@ -1,7 +0,0 @@
-﻿SET IDENTITY_INSERT [dbo].[Herramienta] ON
-INSERT INTO [dbo].[Herramienta] ([Id], [Nombre], [Material], [Precio], [TiempoReparacion], [FabricanteId]) VALUES (1, N'Taladro percutor Bosch GSB 13 RE', N'Acero y plástico', 89.99, 5, 1)
-INSERT INTO [dbo].[Herramienta] ([Id], [Nombre], [Material], [Precio], [TiempoReparacion], [FabricanteId]) VALUES (2, N'Sierra circular Makita HS7601', N'Aluminio y acero', 120.5, 7, 2)
-INSERT INTO [dbo].[Herramienta] ([Id], [Nombre], [Material], [Precio], [TiempoReparacion], [FabricanteId]) VALUES (3, N'Llave inglesa Stanley 300mm', N'Acero al cromo-vanadio', 25.75, 2, 3)
-INSERT INTO [dbo].[Herramienta] ([Id], [Nombre], [Material], [Precio], [TiempoReparacion], [FabricanteId]) VALUES (4, N'Martillo de carpintero Bellota', N'Madera y acero', 15.9, 1, 4)
-INSERT INTO [dbo].[Herramienta] ([Id], [Nombre], [Material], [Precio], [TiempoReparacion], [FabricanteId]) VALUES (5, N'Destornillador eléctrico Black+Decker', N'Plástico y acero', 45.3, 3, 5)
-SET IDENTITY_INSERT [dbo].[Herramienta] OFF
