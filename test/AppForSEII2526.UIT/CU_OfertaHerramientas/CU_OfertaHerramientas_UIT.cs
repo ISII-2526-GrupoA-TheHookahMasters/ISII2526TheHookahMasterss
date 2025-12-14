74fdbe1bd5b3a8e1ed22b3b0a9556426e60d9f6e
IvanGomez11 <ivan.gomez11@alu.uclm.es>
2025-12-14 10:43:17 +0100

    Implementadas todas las pruebas de las interfaces con Selenium

--- test/AppForSEII2526.UIT/CU_OfertaHerramientas/CU_OfertaHerramientas_UIT.cs
+++ test/AppForSEII2526.UIT/CU_OfertaHerramientas/CU_OfertaHerramientas_UIT.cs
@@ -1,5 +1,6 @@
 ﻿using AppForMovies.UIT.Shared;
 using AppForSEII2526.UIT.CU_OfertaHerramientas;
+using OpenQA.Selenium.DevTools.V137.Storage;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,8 @@ namespace AppForSEII2526.UIT.CU_OfertaHerramientas_UIT
     public class CU_OfertaHerramientas_UIT : UC_UIT
     {
         private SelectHerramientasForOferta_PO selectHerramientasForOferta_PO;
+        private DetailOferta_PO detailOferta_PO;
+        private CrearCompra_PO crearCompra_PO;
         private const string herramientaId1 = "1";
         private const string herramientaNombre1 = "Taladro percutor Bosch GSB 13 RE";
         private const string herramientaMaterial1 = "Acero y plástico";
@@ -21,10 +24,14 @@ namespace AppForSEII2526.UIT.CU_OfertaHerramientas_UIT
         private const string herramientaMaterial2 = "Aluminio y acero";
         private const string herramientaPrecio2 = "120,5";
         private const string herramientaFabricante2 = "Makita";
+        private const string porcentaje1 = "-4";
+        private const string porcentaje2 = "105";
 
         public CU_OfertaHerramientas_UIT(ITestOutputHelper output) : base(output)
         {
             selectHerramientasForOferta_PO = new SelectHerramientasForOferta_PO(_driver, _output);
+            detailOferta_PO = new DetailOferta_PO(_driver, _output);
+            crearCompra_PO = new CrearCompra_PO(_driver, _output);
         }
 
         /*private void Precondition_perform_login()
@@ -46,23 +53,121 @@ namespace AppForSEII2526.UIT.CU_OfertaHerramientas_UIT
         }
 
         [Theory]
+        [InlineData(herramientaId1, herramientaNombre1)]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC3_1_BF_CrearOferta(string herramientaId, string herramientaNombre)
+        {
+            InitialStepsForOfertaHerramientas();
+            Thread.Sleep(500);
+            selectHerramientasForOferta_PO.AddHerramientaToOfertaCart(herramientaNombre);
+            Thread.Sleep(500);
+            selectHerramientasForOferta_PO.crearOfertaCarrito();
+            Thread.Sleep(1000);
+            crearCompra_PO.addAtributosOferta(herramientaId, DateTime.Today.AddDays(3).ToString(), DateTime.Today.AddDays(11).ToString(), "PayPal", "Socios", "10");
+            Thread.Sleep(1000);
+            crearCompra_PO.guardarOfertaDialog();
+            Thread.Sleep(1000);
+            Assert.True(detailOferta_PO.CheckOfertaDetail(DateTime.Today.AddDays(3), DateTime.Today.AddDays(11), DateTime.Today, "PayPal", "Socios", 1));
+        }
+
+        [Theory]
         [InlineData(herramientaId1, herramientaNombre1, herramientaMaterial1, herramientaPrecio1, herramientaFabricante1, "Bosch", "")]
         [InlineData(herramientaId2, herramientaNombre2, herramientaMaterial2, herramientaPrecio2, herramientaFabricante2, "", "120,5")]
+        [InlineData(herramientaId1, herramientaNombre1, herramientaMaterial1, herramientaPrecio1, herramientaFabricante1, "Bosch", "89,99")]
         [Trait("LevelTesting", "Funcional Testing")]
-        public void UC3_2_3_AF0_filteringPorPrecioFabricante(string herramientaId, string herramientaNombre, string herramientaMaterial, string herramientaPrecio, string herramientaFabricante,
+        public void UC3_2_3_4_AF0_filtroPorFabricantePrecio(string herramientaId, string herramientaNombre, string herramientaMaterial, string herramientaPrecio, string herramientaFabricante,
             string filtroFabricante, string filtroPrecio)
         {
-            //Arrange
             InitialStepsForOfertaHerramientas();
             var expectedHerramientas = new List<string[]> { new string[] { herramientaId, herramientaNombre, herramientaMaterial, herramientaPrecio, herramientaFabricante }, };
-
-            //Act
             selectHerramientasForOferta_PO.SearchHerramientas(filtroFabricante, filtroPrecio);
-
             Thread.Sleep(500);
-
-            //Assert
             Assert.True(selectHerramientasForOferta_PO.CheckListOfHerramientas(expectedHerramientas));
+        }
+
+        [Theory]
+        [InlineData(herramientaId1, herramientaNombre1, "26/12/2025", "14/12/2025", "Error! Tu oferta debe terminar después de que empiece")]
+        [InlineData(herramientaId1, herramientaNombre1, "09/12/2025", "14/12/2025", "Error! La fecha de inicio de tu oferta debe ser posterior a hoy")]
+        [InlineData(herramientaId1, herramientaNombre1, "14/12/2025", "17/12/2025", "¡Error!, la oferta debe durar al menos una semana")]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC3_5_6_7_AF1_FechasErroneas(string herramientaId, string herramientaNombre, string fechaInicio, string fechaFinal, string expectedError)
+        {
+            InitialStepsForOfertaHerramientas();
+            Thread.Sleep(2000);
+            selectHerramientasForOferta_PO.AddHerramientaToOfertaCart(herramientaNombre);
+            Thread.Sleep(2000);
+            selectHerramientasForOferta_PO.crearOfertaCarrito();
+            Thread.Sleep(2000);
+            crearCompra_PO.addAtributosOferta(herramientaId, fechaInicio, fechaFinal, "Efectivo", "Socios", "10");
+            Thread.Sleep(2000);
+            crearCompra_PO.guardarOfertaDialog();
+            Thread.Sleep(1000);
+
+            Assert.True(crearCompra_PO.CheckValidationError(expectedError), $"Expected error: {expectedError}");
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC3_AF2_ModificarCarrito()
+        {
+            InitialStepsForOfertaHerramientas();
+            selectHerramientasForOferta_PO.AddHerramientaToOfertaCart(herramientaNombre1);
+            selectHerramientasForOferta_PO.AddHerramientaToOfertaCart(herramientaNombre2);
+            Thread.Sleep(1000);
+            selectHerramientasForOferta_PO.crearOfertaCarrito();
+            Thread.Sleep(500);
+            crearCompra_PO.modificarHerramientas();
+            Thread.Sleep(500);
+            selectHerramientasForOferta_PO.RemoveHerramientaFromOfertaCart(herramientaNombre1);
+            Thread.Sleep(500);
+            selectHerramientasForOferta_PO.crearOfertaCarrito();
+            Thread.Sleep(500);
+            var expectedOfertaItems = new List<string[]> { new string[] { herramientaNombre2, herramientaMaterial2, herramientaPrecio2 }, };
+
+            Assert.True(crearCompra_PO.CheckListOfOfertaItems(expectedOfertaItems));
+        }
+
+        [Theory]
+        [InlineData(herramientaId1, herramientaNombre1, porcentaje1, "El porcentaje debe estar entre 0 y 100")]
+        [InlineData(herramientaId1, herramientaNombre1, porcentaje2, "El porcentaje debe estar entre 0 y 100")]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC3_8_9_AF3_PorcentajeErroneo(string herramientaId, string herramientaNombre, string porcentaje, string expectedError)
+        {
+            InitialStepsForOfertaHerramientas();
+            Thread.Sleep(500);
+            selectHerramientasForOferta_PO.AddHerramientaToOfertaCart(herramientaNombre);
+            Thread.Sleep(500);
+            selectHerramientasForOferta_PO.crearOfertaCarrito();
+            Thread.Sleep(2000);
+            crearCompra_PO.addAtributosOferta(herramientaId, DateTime.Today.AddDays(5).ToString("dd/MM/yyyy"),
+                DateTime.Today.AddDays(15).ToString("dd/MM/yyyy"), "Tarjeta de Crédito", "Clientes", porcentaje);
+            Thread.Sleep(2000);
+            crearCompra_PO.guardarOfertaDialog();
+            Thread.Sleep(1000);
+
+            Assert.True(crearCompra_PO.CheckValidationError(expectedError), $"Expected error: {expectedError}");
+        }
+
+        
+
+        [Theory]
+        [InlineData(herramientaId1, herramientaNombre1, "16/12/2025", "", "Error! Fecha Final es un campo obligatorio")]
+        [InlineData(herramientaId1, herramientaNombre1, "", "26/12/2025", "Error! Fecha Inicio es un campo obligatorio")]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC3_11_12_AF5_CamposObligatorios(string herramientaId, string herramientaNombre, string fechaInicio, string fechaFinal, string expectedError)
+        {
+            InitialStepsForOfertaHerramientas();
+            Thread.Sleep(2000);
+            selectHerramientasForOferta_PO.AddHerramientaToOfertaCart(herramientaNombre);
+            Thread.Sleep(2000);
+            selectHerramientasForOferta_PO.crearOfertaCarrito();
+            Thread.Sleep(2000);
+            crearCompra_PO.addAtributosOferta(herramientaId, fechaInicio, fechaFinal, "Efectivo", "Socios", "10");
+            Thread.Sleep(2000);
+            crearCompra_PO.guardarOfertaDialog();
+            Thread.Sleep(1000);
+
+            Assert.True(crearCompra_PO.CheckValidationError(expectedError), $"Expected error: {expectedError}");
         }
     }
 }
---
+++ test/AppForSEII2526.UIT/CU_OfertaHerramientas/CrearCompra_PO.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.CU_OfertaHerramientas
+{
+    public class CrearCompra_PO : PageObject
+    {
+        By buttonCrearOferta = By.Id("crearOfertaButton");
+        By button_DialogOK = By.Id("Button_DialogOK");
+        By modifyHerramientasButton = By.Id("modifyHerramientasButton");
+        By tableOfOfertaItems = By.Id("TableOfOfertaItems");
+
+        public CrearCompra_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+
+        public void crearOferta()
+        {
+            WaitForBeingClickable(buttonCrearOferta);
+            _driver.FindElement(buttonCrearOferta).Click();
+        }
+
+        public void guardarOfertaDialog()
+        {
+            WaitForBeingClickable(button_DialogOK);
+            _driver.FindElement(button_DialogOK).Click();
+        }
+
+        public void modificarHerramientas()
+        {
+            WaitForBeingClickable(modifyHerramientasButton);
+            _driver.FindElement(modifyHerramientasButton).Click();
+        }
+
+        public void addAtributosOferta(string herramientaId, string fechaInicio, string fechaFinal, string metodoPago, string dirigidaA, string porcentaje)
+        {
+            WaitForBeingClickable(By.Id("inputFechaInicio"));
+            _driver.FindElement(By.Id("inputFechaInicio")).SendKeys(fechaInicio);
+
+            WaitForBeingClickable(By.Id("inputFechaFinal"));
+            _driver.FindElement(By.Id("inputFechaFinal")).SendKeys(fechaFinal);
+
+            if (metodoPago == "") metodoPago = "Tarjeta de Crédito";
+            SelectElement selectElement = new SelectElement(_driver.FindElement(By.Id("selectTipoMetodoPago")));
+            selectElement.SelectByText(metodoPago);
+
+            if (dirigidaA == "") dirigidaA = "Clientes";
+            SelectElement selectElement2 = new SelectElement(_driver.FindElement(By.Id("selectTipoDirigidaOferta")));
+            selectElement2.SelectByText(dirigidaA);
+
+            WaitForBeingClickable(By.Id($"porcentaje_{herramientaId}"));
+            //var inputPorcentaje = 
+            _driver.FindElement(By.Id($"porcentaje_{herramientaId}")).Clear();
+            _driver.FindElement(By.Id($"porcentaje_{herramientaId}")).SendKeys(porcentaje);
+
+
+            _driver.FindElement(buttonCrearOferta).Click();
+
+        }
+
+        public bool CheckListOfOfertaItems(List<string[]> expectedOfertaItems)
+        {
+            return CheckBodyTable(expectedOfertaItems, tableOfOfertaItems);
+        }
+
+        public bool CheckValidationError(string expectedError)
+        {
+            return _driver.PageSource.Contains(expectedError);
+        }
+    }
+}
