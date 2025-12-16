a20f462e52478d5390493f82354fec3d06eb4500
Iván Gómez <ivan.gomez11@alu.uclm.es>
2025-12-16 21:32:58 +0100

    Cambiado el despliegue de la Web de Azure a localhost para el examen … (#233)
    Cambiado el despliegue de la Web de Azure a localhost para el examen y añadido el Plan de Pruebas

--- src/AppForSEII2526.API/Controllers/ReparacionController.cs
+++ src/AppForSEII2526.API/Controllers/ReparacionController.cs
@@ -24,7 +24,7 @@ namespace AppForSEII2526.API.Controllers
 
         [HttpGet]
         [Route("[action]")]
-        [ProducesResponseType(typeof(IList<ReparacionDetailDTO>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ReparacionDetailDTO), (int)HttpStatusCode.OK)]
         public async Task<ActionResult> GetReparacionesPorId(int id)
         {
             var reparacion = await _context.Reparacion
