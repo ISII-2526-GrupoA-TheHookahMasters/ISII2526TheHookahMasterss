4eb40f728f1884af52c2679bf37474e8d6f4bced
Kikolonx <joseenrique.ortiz@alu.uclm.es>
2025-12-16 21:29:48 +0100

    Hechas pruebas de unidad del caso de uso Reparar Herramientas (#232)
    Co-authored-by: IvanGomez11 <ivan.gomez11@alu.uclm.es>

---
+++ test/AppForSEII2526.UIT/CU_ReparacionHerramientas/CU_ReparacionHerramientas_UIT.cs
@@ -0,0 +1,182 @@
+﻿using AppForMovies.UIT.Shared;
+using AppForSEII2526.UIT.CU_RepararHerramientas;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.CU_RepararHerramientas
+{
+        public class CU_ReparacionHerramientas_UIT : UC_UIT
+        {
+            private SelectHerramientasForReparacion_PO selectHerramientasForReparacion_PO;
+            private DetailReparacion_PO detailReparacion_PO;
+            private CrearReparacion_PO crearReparacion_PO;
+            private const string herramientaId1 = "1";
+            private const string herramientaNombre1 = "Taladro percutor Bosch GSB 13 RE";
+            private const string herramientaMaterial1 = "Acero y plástico";
+            private const string herramientaPrecio1 = "89,99";
+            private const string herramientaFabricante1 = "Bosch";
+            private const string herramientaTiempoReparacion1 = "5";
+            private const string herramientaId2 = "2";
+            private const string herramientaNombre2 = "Sierra circular Makita HS7601";
+            private const string herramientaMaterial2 = "Aluminio y acero";
+            private const string herramientaPrecio2 = "120,5";
+            private const string herramientaFabricante2 = "Makita";
+            private const string herramientaTiempoReparacion2 = "7";
+
+
+        public CU_ReparacionHerramientas_UIT(ITestOutputHelper output) : base(output)
+            {
+            selectHerramientasForReparacion_PO = new SelectHerramientasForReparacion_PO(_driver, _output);
+                detailReparacion_PO = new DetailReparacion_PO(_driver, _output);
+                crearReparacion_PO = new CrearReparacion_PO(_driver, _output);
+            }
+
+            /*private void Precondition_perform_login()
+            {
+                Perform_login("elena@uclm.es", "Password1234%");
+            }*/
+
+            private void InitialStepsForReparacionHerramientas()
+            {
+                Initial_step_opening_the_web_page();
+
+                //Precondition_perform_login();
+                By id = By.Id("CrearReparacion");
+                selectHerramientasForReparacion_PO.WaitForBeingVisible(id);
+
+                Thread.Sleep(500);
+
+                _driver.FindElement(id).Click();
+            }
+
+            [Theory]
+            [InlineData(herramientaId1, herramientaNombre1, herramientaPrecio1)]
+            [Trait("LevelTesting", "Funcional Testing")]
+            public void UC2_1_BF_CrearReparacion(string herramientaId, string herramientaNombre, string herramientaPrecio1)
+            {
+                InitialStepsForReparacionHerramientas();
+                Thread.Sleep(500);
+                selectHerramientasForReparacion_PO.AddHerramientaToReparacionCart(herramientaId);
+                Thread.Sleep(500);
+                selectHerramientasForReparacion_PO.crearReparacionCarrito();
+                Thread.Sleep(1000);
+                crearReparacion_PO.addAtributosReparacion(herramientaId, herramientaNombre, "Carlos", "Gomez", DateTime.Today.AddDays(1).ToString(), "Tarjeta de Crédito", "1", "Rotura");
+                Thread.Sleep(300);
+                crearReparacion_PO.pulsarCrearReparacion();
+                Thread.Sleep(1000);
+                crearReparacion_PO.guardarReparacionDialog();
+                Thread.Sleep(1000);
+                Assert.True(detailReparacion_PO.CheckReparacionDetail( "Carlos" , "Gomez", DateTime.Today.AddDays(1).ToString("dd/MM/yyyy"), DateTime.Today.AddDays(6).ToString("dd/MM/yyyy"), herramientaPrecio1, "1"));
+            }
+
+            [Theory]
+            [InlineData(herramientaId1, herramientaNombre1, herramientaMaterial1, herramientaPrecio1, herramientaFabricante1, "Taladro percutor Bosch GSB 13 RE", "")]
+            [InlineData(herramientaId2, herramientaNombre2, herramientaMaterial2, herramientaPrecio2, herramientaFabricante2, "", "7")]
+            [InlineData(herramientaId1, herramientaNombre1, herramientaMaterial1, herramientaPrecio1, herramientaFabricante1, "Taladro percutor Bosch GSB 13 RE", "5")]
+            [Trait("LevelTesting", "Funcional Testing")]
+            public void UC2_2_3_4_AF0_filtroPorFabricantePrecio(string herramientaId, string herramientaNombre, string herramientaMaterial, string herramientaPrecio, string herramientaFabricante,
+                string filtroNombre, string filtroTiempoReparacion)
+            {
+                InitialStepsForReparacionHerramientas();
+                Thread.Sleep(1000);
+                var expectedHerramientas = new List<string[]> { new string[] { herramientaId, herramientaNombre, herramientaMaterial, herramientaPrecio, herramientaFabricante }, };
+                selectHerramientasForReparacion_PO.SearchHerramientas(filtroNombre, filtroTiempoReparacion);
+                Thread.Sleep(1000);
+                Assert.True(selectHerramientasForReparacion_PO.CheckListOfHerramientas(expectedHerramientas));
+            }
+
+            [Theory]
+            [InlineData(herramientaId1, herramientaNombre1, "12/12/2025", "Error! La fecha de entrega debe ser posterior a hoy")]
+            [Trait("LevelTesting", "Funcional Testing")]
+            public void UC2_5_6_7_AF1_FechasErroneas(string herramientaId, string herramientaNombre, string fechaEntrega, string expectedError)
+            {
+                InitialStepsForReparacionHerramientas();
+                Thread.Sleep(1000);
+                selectHerramientasForReparacion_PO.AddHerramientaToReparacionCart(herramientaId);
+                Thread.Sleep(1000);
+                selectHerramientasForReparacion_PO.crearReparacionCarrito();
+                Thread.Sleep(1000);
+                crearReparacion_PO.addAtributosReparacion(herramientaId, herramientaNombre, "Carlos", "Gomez", fechaEntrega, "Tarjeta de Crédito", "1", "Rotura");
+                Thread.Sleep(300);
+                crearReparacion_PO.pulsarCrearReparacion();
+                Thread.Sleep(1000);
+                crearReparacion_PO.guardarReparacionDialog();
+                Thread.Sleep(1000);
+
+                Assert.True(crearReparacion_PO.CheckValidationError(expectedError), $"Expected error: {expectedError}");
+            }
+
+            [Fact]
+            [Trait("LevelTesting", "Funcional Testing")]
+            public void UC2_AF2_ModificarCarrito()
+            {
+                InitialStepsForReparacionHerramientas();
+                selectHerramientasForReparacion_PO.AddHerramientaToReparacionCart(herramientaId1);
+                selectHerramientasForReparacion_PO.AddHerramientaToReparacionCart(herramientaId2);
+                Thread.Sleep(1000);
+                selectHerramientasForReparacion_PO.crearReparacionCarrito();
+                Thread.Sleep(500);
+                crearReparacion_PO.modificarHerramientas();
+                Thread.Sleep(500);
+                selectHerramientasForReparacion_PO.RemoveHerramientaFromReparacionCart(herramientaNombre1);
+                Thread.Sleep(500);
+                selectHerramientasForReparacion_PO.crearReparacionCarrito();
+                Thread.Sleep(500);
+                var expectedReparacionItems = new List<string[]> { new string[] { herramientaNombre2, herramientaTiempoReparacion2, herramientaPrecio2 }, };
+
+                Assert.True(crearReparacion_PO.CheckListOfReparacionItems(expectedReparacionItems));
+            }
+
+        
+
+
+
+            [Theory]
+            [InlineData(herramientaId1, herramientaNombre1, "", "Gomez", "20/12/2025", "2", "The NombreCliente field is required.")]
+            [InlineData(herramientaId1, herramientaNombre1, "Carlos", "", "20/12/2025", "2", "The ApellidosCliente field is required.")]
+            [Trait("LevelTesting", "Funcional Testing")]
+            public void UC2_11_12_AF5_CamposObligatorios(string herramientaId, string herramientaNombre, string nombreCliente, string apellidoCliente, string fechaEntrega, string cantidad, string expectedError)
+            {
+                InitialStepsForReparacionHerramientas();
+                Thread.Sleep(1000);
+                selectHerramientasForReparacion_PO.AddHerramientaToReparacionCart(herramientaId);
+                Thread.Sleep(1000);
+                selectHerramientasForReparacion_PO.crearReparacionCarrito();
+                Thread.Sleep(1000);
+                crearReparacion_PO.addAtributosReparacion(herramientaId, herramientaNombre, nombreCliente, apellidoCliente, fechaEntrega, "Tarjeta de Crédito", cantidad, "Rotura");
+                Thread.Sleep(300);
+                crearReparacion_PO.pulsarCrearReparacion();
+                Thread.Sleep(1000);
+            /*
+                crearReparacion_PO.guardarReparacionDialog();
+                Thread.Sleep(1000);
+            */
+            Assert.True(crearReparacion_PO.CheckValidationError(expectedError), $"Expected error: {expectedError}");
+            }
+
+            [Theory]
+            [InlineData(herramientaId1, herramientaNombre1, "Carlos", "Gomez", "20/12/2025", "0")]
+            [Trait("LevelTesting", "Funcional Testing")]
+            public void UC2_11_12_AF5_CantidadErronea(string herramientaId, string herramientaNombre, string nombreCliente, string apellidoCliente, string fechaEntrega, string cantidad)
+            {
+                InitialStepsForReparacionHerramientas();
+                Thread.Sleep(2000);
+                selectHerramientasForReparacion_PO.AddHerramientaToReparacionCart(herramientaId);
+                Thread.Sleep(2000);
+                selectHerramientasForReparacion_PO.crearReparacionCarrito();
+                Thread.Sleep(2000);
+                crearReparacion_PO.addAtributosReparacion(herramientaId, herramientaNombre, nombreCliente, apellidoCliente, fechaEntrega, "Tarjeta de Crédito", cantidad, "Rotura");
+                Thread.Sleep(300);
+                
+
+                Assert.False(crearReparacion_PO.IsSubmitEnabled());
+            }
+
+        }
+}
+
+
---
+++ test/AppForSEII2526.UIT/CU_ReparacionHerramientas/SelectHerramientasForReparacion_PO.cs
@@ -0,0 +1,79 @@
+﻿using AppForMovies.UIT.Shared;
+using AppForSEII2526.UIT.CU_RepararHerramientas;
+using OpenQA.Selenium.DevTools.V137.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.CU_RepararHerramientas
+{
+    public class SelectHerramientasForReparacion_PO : PageObject
+    {
+        By inputTiempoReparacion = By.Id("inputTiempoReparacion");
+        By inputNombreHerramienta = By.Id("inputNombreHerramienta");
+        By buttonSearchHerramientas = By.Id("searchHerramientas");
+        By tableOfHerramientasBy = By.Id("TableOfHerramientas");
+        By errorShownBy = By.Id("ErrorsShown");
+        By buttonCrearReparacionCarrito = By.Id("crearReparacionCarritoButton");
+
+
+        public SelectHerramientasForReparacion_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+        public void SearchHerramientas(string nombreHerramienta, string tiempoReparacion)
+        {
+            //wait for the webelement to be clickable
+            WaitForBeingClickable(inputTiempoReparacion);
+            _driver.FindElement(inputTiempoReparacion).SendKeys(tiempoReparacion);
+            Thread.Sleep(1000);
+            WaitForBeingClickable(inputNombreHerramienta);
+            _driver.FindElement(inputNombreHerramienta).SendKeys(nombreHerramienta);
+            Thread.Sleep(1000);
+            _driver.FindElement(buttonSearchHerramientas).Click();
+        }
+
+        public void crearReparacionCarrito()
+        {
+            WaitForBeingClickable(buttonCrearReparacionCarrito);
+            _driver.FindElement(buttonCrearReparacionCarrito).Click();
+        }
+
+        public bool CheckListOfHerramientas(List<string[]> expectedHerramientas)
+        {
+            return CheckBodyTable(expectedHerramientas, tableOfHerramientasBy);
+        }
+
+        public bool CheckMessageError(string errorMessage)
+        {
+            IWebElement actualErrorShown = _driver.FindElement(errorShownBy);
+            _output.WriteLine($"actual Message shown:{actualErrorShown.Text}");
+            return actualErrorShown.Text.Contains(errorMessage);
+        }
+
+        public void AddHerramientaToReparacionCart(string herramientaId)
+        {
+            WaitForBeingClickable(By.Id("herramientaParaReparar_" + herramientaId));
+            _driver.FindElement(By.Id("herramientaParaReparar_" + herramientaId)).Click();
+        }
+
+        public void RemoveHerramientaFromReparacionCart(string herramientaNombre)
+        {
+            WaitForBeingClickable(By.Id("removeHerramienta_" + herramientaNombre));
+            _driver.FindElement(By.Id("removeHerramienta_" + herramientaNombre)).Click();
+        }
+
+        public bool ReparacionNotAvailable()
+        {
+            try
+            {
+                return _driver.FindElement(buttonCrearReparacionCarrito).Displayed == false;
+            }
+            catch (Exception e)
+            {
+                return true;
+            }
+        }
+    }
+}
